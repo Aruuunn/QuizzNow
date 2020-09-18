1bfcb77ccb6db1a96d2f4cd740577218ee05b4c3
Arun <arunmurugan200@gmail.com>
2020-09-18 15:55:13 +0530

    first commit

---
+++ server/config/domain.ts
@@ -0,0 +1,2 @@
+
+export const BaseURL = 'http://localhost:5000/api';
---
+++ server/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { UserService } from './user/user.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService, UserService],
+})
+export class AppModule {}
