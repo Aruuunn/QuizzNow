c9c7d5117e8b44691dbadf2ecf763ef8b063a53a
Arun Murugan <arunmurugan200@gmail.com>
2020-10-31 15:12:38 +0530

    Improvements

--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -83,15 +83,65 @@ class Home extends Component<Props, State> {
           )}
         </NavBar>
         {this.state.loading ? "Loading..." : null}
-        <Container style={{marginTop:'30px'}}>
+        <Container style={{ marginTop: "30px" }}>
+          <Typography
+            variant="h4"
+            component="h1"
+            style={{
+              color: "#E578C1",
+              marginBottom: "30px",
+              marginLeft: "10px",
+              fontFamily: "'Fredoka One', cursive",
+            }}
+          >
+            Your Quizzes
+          </Typography>
           {(this.state.quizzes as Quiz[]).map((o, i) => (
-            <Card key={i} style={{padding:'10px',color:'white',margin:'10px',maxWidth:'500px'}}>
+            <Card
+              key={i}
+              style={{
+                padding: "10px",
+                color: "white",
+                margin: "10px",
+                maxWidth: "500px",
+              }}
+            >
               <CardContent>
                 <Typography variant="h5">{o.title}</Typography>
+                <div style={{ color: "grey", marginTop: "10px" }}>
+                  <Typography variant="body1">
+                    Start: {o.startDatetime}
+                  </Typography>
+                  <Typography variant="body1">End: {o.endDatetime}</Typography>
+                </div>
               </CardContent>
             </Card>
           ))}
+          {this.state.quizzes.length === 0 && (
+            <div
+              style={{ color: "black", marginTop: "10vh", marginLeft: "10px" }}
+            >
+              {" "}
+              <Typography
+                variant="h3"
+                style={{ fontFamily: "'Fredoka One', cursive" }}
+              >
+                Nothing Here....Yet
+              </Typography>
+              <Typography
+                style={{
+                  fontFamily: "'Fredoka One', cursive",
+                }}
+                variant="h4"
+              >
+                The Quizzes you created will show up here
+              </Typography>
+            </div>
+          )}
         </Container>
+        <svg  style={{position:'fixed',bottom:0,left:0,zIndex:-1}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#072540" fill-opacity="1" d="M0,224L48,208C96,192,192,160,288,160C384,160,480,192,576,218.7C672,245,768,267,864,272C960,277,1056,267,1152,240C1248,213,1344,171,1392,149.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+</svg>
       </div>
     );
   }
