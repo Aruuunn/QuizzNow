cf21fed24c9f1494c530ef62b35c491d405d4f95
Arun Murugan <arunmurugan200@gmail.com>
2020-11-13 17:07:44 +0530

    Started working on feature to add question

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -8,12 +8,11 @@ const theme = createMuiTheme({
     secondary: {
       main: "#E578C1",
       dark: "#9C4668",
-      contrastText:'white'
     },
     background: {
       default: "#183D5D",
       paper: "#072540",
-    },
+    }, 
   },
 });
 
